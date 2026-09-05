6bece0f1f4c1ffeeb4a2a700fa16dd133751f7b6
jagadishjaggu7 <132246274+jagadishjaggu7@users.noreply.github.com>
2026-09-05 18:02:34 +0530

    Wire dashboard and new request navigation

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -13,7 +13,14 @@ import AssignmentRounded from "@mui/icons-material/AssignmentRounded";
 import ListAltRounded from "@mui/icons-material/ListAltRounded";
 import AddCircleOutlineRounded from "@mui/icons-material/AddCircleOutlineRounded";
 
-export default function Sidebar() {
+export type AppPage = "dashboard" | "new-request";
+
+interface SidebarProps {
+  activePage: AppPage;
+  onNavigate: (page: AppPage) => void;
+}
+
+export default function Sidebar({ activePage, onNavigate }: SidebarProps) {
   return (
     <Box
       sx={{
@@ -35,32 +42,32 @@ export default function Sidebar() {
       <Divider />
 
       <List sx={{ px: 1, py: 2 }}>
-        <ListItemButton selected sx={{ borderRadius: 2, mb: 1 }}>
-          <ListItemIcon>
-            <DashboardRounded color="primary" />
-          </ListItemIcon>
+        <ListItemButton
+          selected={activePage === "dashboard"}
+          onClick={() => onNavigate("dashboard")}
+          sx={{ borderRadius: 2, mb: 1 }}
+        >
+          <ListItemIcon><DashboardRounded color="primary" /></ListItemIcon>
           <ListItemText primary="Dashboard" />
         </ListItemButton>
 
-        <ListItemButton sx={{ borderRadius: 2, mb: 1 }}>
-          <ListItemIcon>
-            <AssignmentRounded />
-          </ListItemIcon>
-          <ListItemText primary="My Requests" />
+        <ListItemButton disabled sx={{ borderRadius: 2, mb: 1 }}>
+          <ListItemIcon><AssignmentRounded /></ListItemIcon>
+          <ListItemText primary="My Requests" secondary="Coming soon" />
         </ListItemButton>
 
-        <ListItemButton sx={{ borderRadius: 2, mb: 1 }}>
-          <ListItemIcon>
-            <ListAltRounded />
-          </ListItemIcon>
-          <ListItemText primary="All Requests" />
+        <ListItemButton disabled sx={{ borderRadius: 2, mb: 1 }}>
+          <ListItemIcon><ListAltRounded /></ListItemIcon>
+          <ListItemText primary="All Requests" secondary="Coming soon" />
         </ListItemButton>
 
-        <ListItemButton sx={{ borderRadius: 2 }}>
-          <ListItemIcon>
-            <AddCircleOutlineRounded />
-          </ListItemIcon>
-          <ListItemText primary="Create CR" />
+        <ListItemButton
+          selected={activePage === "new-request"}
+          onClick={() => onNavigate("new-request")}
+          sx={{ borderRadius: 2 }}
+        >
+          <ListItemIcon><AddCircleOutlineRounded color={activePage === "new-request" ? "primary" : undefined} /></ListItemIcon>
+          <ListItemText primary="New Request" />
         </ListItemButton>
       </List>
     </Box>
