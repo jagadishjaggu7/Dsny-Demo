c2c11ca11314c954c8b1d629adf6a50860f63b54
jagadishjaggu7 <132246274+jagadishjaggu7@users.noreply.github.com>
2026-09-05 18:02:49 +0530

    Pass active page navigation through main layout

--- src/layouts/MainLayout.tsx
+++ src/layouts/MainLayout.tsx
@@ -1,20 +1,21 @@
 import { Box } from "@mui/material";
-import Sidebar from "../components/Sidebar";
+import Sidebar, { type AppPage } from "../components/Sidebar";
 import TopBar from "../components/TopBar";
 import type { ReactNode } from "react";
 
 interface Props {
   children: ReactNode;
+  activePage: AppPage;
+  onNavigate: (page: AppPage) => void;
 }
 
-export default function MainLayout({ children }: Props) {
+export default function MainLayout({ children, activePage, onNavigate }: Props) {
   return (
     <Box sx={{ display: "flex", minHeight: "100vh", bgcolor: "background.default" }}>
-      <Sidebar />
+      <Sidebar activePage={activePage} onNavigate={onNavigate} />
 
-      <Box sx={{ flexGrow: 1 }}>
+      <Box sx={{ flexGrow: 1, minWidth: 0 }}>
         <TopBar />
-
         <Box sx={{ p: 3 }}>{children}</Box>
       </Box>
     </Box>
